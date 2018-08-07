df0f9eb722df5648414fefc4f87b87898e3b4b0c
musi <957028613@qq.com>
2018-08-08 00:37:51 +0800

    添加ajax

--- jQuery.js
+++ jQuery.js
@@ -24,11 +24,39 @@ window.jQuery = function(nodeOrSelector) {
             nodes[i].textContent = text          
         }
     }
+
     return nodes
 }
 
-window.$ = jQuery
+window.jQuery.ajax = function({url, method, body, hearders}) {
+    return new Promise(function(resolve, reject) {
+        let request = new XMLHttpRequest()
+        request.open(method, url)
+        for (let key in hearders) {
+            let value = hearders[key]
+            request.setRequestHeader(key, value)    
+        }
+        request.onreadystatechange = function() {
+            if (request.readyState === 4) {
+                if (request.status >= 200 && request.status < 300) {
+                    resolve.call(undefined, request.responseText)
+                } else if (request.status >= 400) {
+                    reject.call(request)
+                }
+            }
+        }
+        request.send(body)
+    })
+    
+}
+
+window.$ = window.jQuery
 var $div = $('div')
 $div.addClass('red') // 可将所有 div 的 class 添加一个 red
 $div.setText('hi') // 可将所有 div 的 textContent 变为 hi
 
+jQuery.ajax({
+    url: '/xxx',
+    method: 'get'
+}).then(success, fail)
+// 封装ajax
